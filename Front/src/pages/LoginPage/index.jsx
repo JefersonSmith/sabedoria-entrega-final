1be27a3d1b27db8c6a97facbb334bbf2e6d60982
Joao Filho <joaofilhomil@gmail.com>
2023-12-26 15:46:46 -0300

    Estilizando LoginPage com css.

--- Front/src/pages/LoginPage/index.jsx
+++ Front/src/pages/LoginPage/index.jsx
@@ -3,43 +3,53 @@ import { Input } from "../../components"
 import { loginFormSchema } from "./loginFormSchema"
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom";
+import "./loginPage.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../../assets/logo.png"
 
 export const LoginPage = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: zodResolver(loginFormSchema),
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginFormSchema),
+  })
 
-    const submit = (payload) => {
-        console.log(payload)
-    }
+  const submit = (payload) => {
+    console.log(payload)
+  }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit(submit)}>
-                <h3>Login</h3>
-                <Input
-                    label="Email"
-                    type="email"
-                    placeholder="Digite seu email..."
-                    {...register("email")}
-                    errors={errors.email}
-                />
+  return (
+    <div className="login-container">
+      <img src={logo} alt="logo" className="logo"/>
+      <form onSubmit={handleSubmit(submit)} className="login-form">
+        <h3 className="login-title">Login</h3>
 
-                <Input
-                    label="Senha"
-                    type="text"
-                    placeholder="Digite sua senha..."
-                    {...register("password")}
-                    errors={errors.password}
-                />
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Digite seu email..."
+          {...register("email")}
+          errors={errors.email}
+        />
 
-                <button type="submit">Acessar</button>
-                <Link to="/register">Cadastre-se</Link>
-            </form>
-        </div>
-    )
+        <Input
+          label="Senha"
+          type="password"
+          placeholder="Digite sua senha..."
+          {...register("password")}
+          errors={errors.password}
+        />
+
+        <button type="submit" className="btn btn-primary mt-3">
+          Acessar
+        </button>
+
+        <Link to="/register" className="register-link">
+          Cadastre-se
+        </Link>
+      </form>
+    </div>
+  )
 }
