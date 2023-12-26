247924fc306873616178b5e7ce82ca6e597cbe39
Joao Filho <joaofilhomil@gmail.com>
2023-12-26 16:10:46 -0300

    Estilizando RegisterPage com css.

--- Front/src/pages/RegisterPage/index.jsx
+++ Front/src/pages/RegisterPage/index.jsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { registerFormSchema } from "./registerFormSchema";
 import { Link } from "react-router-dom";
+import "./registerPage.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 export const RegisterPage = () => {
@@ -16,46 +19,45 @@ export const RegisterPage = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(submit)}>
-                <h3>Cadastro de Usuário</h3>
+        <div className="register-container">
+      <form onSubmit={handleSubmit(submit)} className="register-form">
+        <h3>Cadastro de Usuário</h3>
 
-                <Input
-                    label="Nome"
-                    type="text"
-                    placeholder="Digite seu nome..."
-                    {...register("name")}
-                    errors={errors.name}
-                />
+        <Input
+          label="Nome"
+          type="text"
+          placeholder="Digite seu nome..."
+          {...register("name")}
+          errors={errors.name}
+        />
 
-                <Input
-                    label="Email"
-                    type="email"
-                    placeholder="Digite seu email..."
-                    {...register("email")}
-                    errors={errors.email}
-                />
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Digite seu email..."
+          {...register("email")}
+          errors={errors.email}
+        />
 
-                <Input
-                    label="Senha"
-                    type="text"
-                    placeholder="Digite sua senha..."
-                    {...register("password")}
-                    errors={errors.password}
-                />
+        <Input
+          label="Senha"
+          type="password"
+          placeholder="Digite sua senha..."
+          {...register("password")}
+          errors={errors.password}
+        />
 
+        <Input
+          label="Confirme sua senha"
+          type="password"
+          placeholder="Digite sua confirmação de senha..."
+          {...register("passwordConfirm")}
+          errors={errors.passwordConfirm}
+        />
 
-                <Input
-                    label="Confirma sua senha"
-                    type="text"
-                    placeholder="Digite sua confirmação senha..."
-                    {...register("passwordConfirm")}
-                    errors={errors.passwordConfirm}
-                />
-
-                <button type="submit">Cadastrar</button>
-                <Link to="/login">Já tenho uma conta</Link>
-            </form>
-        </div>
+        <button type="submit" className="btn btn-primary">Cadastrar</button>
+        <Link to="/login" className="d-block mt-2">Já tenho uma conta</Link>
+      </form>
+    </div>
     )
 }
